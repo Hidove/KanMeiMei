0fe2061ab78360d62d54a3f26dfd154068b23078
Hidove <i@abcyun.cc>
2020-01-04 22:06:47 +0800

    补充说明文件

---
+++ meimei.sql
@@ -0,0 +1,50 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 本地
+ Source Server Type    : MySQL
+ Source Server Version : 50553
+ Source Host           : localhost:3306
+ Source Schema         : meimei
+
+ Target Server Type    : MySQL
+ Target Server Version : 50553
+ File Encoding         : 65001
+
+ Date: 04/01/2020 20:20:52
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for meimei_image
+-- ----------------------------
+DROP TABLE IF EXISTS `meimei_image`;
+CREATE TABLE `meimei_image`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `parent_id` int(10) NOT NULL COMMENT '图片源id',
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '图片主题',
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `key` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `createtime` bigint(20) NOT NULL,
+  `updatetime` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`, `url`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for meimei_list
+-- ----------------------------
+DROP TABLE IF EXISTS `meimei_list`;
+CREATE TABLE `meimei_list`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '标题',
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL COMMENT '采集地址',
+  `key` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '注明是哪个网站的',
+  `createtime` bigint(20) NOT NULL COMMENT '创建时间',
+  `updatetime` bigint(20) NOT NULL COMMENT '执行时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `url`(`url`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
